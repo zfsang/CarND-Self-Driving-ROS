6393f9f2f50d8b9e6e855c1c3acf0501be3a9570
zfsang <zfsang@gmail.com>
2020-01-25 23:47:32 +0000

    feat:add ros module

---
+++ ros/devel/share/gennodejs/ros/dbw_mkz_msgs/msg/_index.js
@@ -0,0 +1,58 @@
+
+"use strict";
+
+let ThrottleCmd = require('./ThrottleCmd.js');
+let BrakeInfoReport = require('./BrakeInfoReport.js');
+let ThrottleReport = require('./ThrottleReport.js');
+let HillStartAssist = require('./HillStartAssist.js');
+let GearReject = require('./GearReject.js');
+let WheelPositionReport = require('./WheelPositionReport.js');
+let WatchdogCounter = require('./WatchdogCounter.js');
+let GearCmd = require('./GearCmd.js');
+let BrakeReport = require('./BrakeReport.js');
+let ParkingBrake = require('./ParkingBrake.js');
+let ThrottleInfoReport = require('./ThrottleInfoReport.js');
+let BrakeCmd = require('./BrakeCmd.js');
+let Wiper = require('./Wiper.js');
+let GearReport = require('./GearReport.js');
+let SteeringCmd = require('./SteeringCmd.js');
+let Misc1Report = require('./Misc1Report.js');
+let WheelSpeedReport = require('./WheelSpeedReport.js');
+let FuelLevelReport = require('./FuelLevelReport.js');
+let AmbientLight = require('./AmbientLight.js');
+let TirePressureReport = require('./TirePressureReport.js');
+let TwistCmd = require('./TwistCmd.js');
+let TurnSignalCmd = require('./TurnSignalCmd.js');
+let SteeringReport = require('./SteeringReport.js');
+let SurroundReport = require('./SurroundReport.js');
+let Gear = require('./Gear.js');
+let TurnSignal = require('./TurnSignal.js');
+
+module.exports = {
+  ThrottleCmd: ThrottleCmd,
+  BrakeInfoReport: BrakeInfoReport,
+  ThrottleReport: ThrottleReport,
+  HillStartAssist: HillStartAssist,
+  GearReject: GearReject,
+  WheelPositionReport: WheelPositionReport,
+  WatchdogCounter: WatchdogCounter,
+  GearCmd: GearCmd,
+  BrakeReport: BrakeReport,
+  ParkingBrake: ParkingBrake,
+  ThrottleInfoReport: ThrottleInfoReport,
+  BrakeCmd: BrakeCmd,
+  Wiper: Wiper,
+  GearReport: GearReport,
+  SteeringCmd: SteeringCmd,
+  Misc1Report: Misc1Report,
+  WheelSpeedReport: WheelSpeedReport,
+  FuelLevelReport: FuelLevelReport,
+  AmbientLight: AmbientLight,
+  TirePressureReport: TirePressureReport,
+  TwistCmd: TwistCmd,
+  TurnSignalCmd: TurnSignalCmd,
+  SteeringReport: SteeringReport,
+  SurroundReport: SurroundReport,
+  Gear: Gear,
+  TurnSignal: TurnSignal,
+};
---
+++ ros/devel/share/gennodejs/ros/styx_msgs/msg/_index.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+let TrafficLightArray = require('./TrafficLightArray.js');
+let TrafficLight = require('./TrafficLight.js');
+let Lane = require('./Lane.js');
+let Waypoint = require('./Waypoint.js');
+
+module.exports = {
+  TrafficLightArray: TrafficLightArray,
+  TrafficLight: TrafficLight,
+  Lane: Lane,
+  Waypoint: Waypoint,
+};
